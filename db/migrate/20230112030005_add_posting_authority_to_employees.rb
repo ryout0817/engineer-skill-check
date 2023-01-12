a5d5e1d8ea3a455c4a8932d9cbee87d14220b63c
Kami-Ryotaro <kamiryo.0817@gmail.com>
2023-01-12 13:01:13 +0000

    [add]追加した従業員のカラムのposting_authorityに設定を追加しました。
    boolean型なのでデフォルトをfalseにしました。

--- db/migrate/20230112030005_add_posting_authority_to_employees.rb
+++ db/migrate/20230112030005_add_posting_authority_to_employees.rb
@@ -1,5 +1,5 @@
 class AddPostingAuthorityToEmployees < ActiveRecord::Migration[6.1]
   def change
-    add_column :employees, :posting_authority, :boolean
+    add_column :employees, :posting_authority, :boolean, default: false
   end
 end
