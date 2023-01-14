9a41f6fec71864b66415d7bb892d4797173283b2
Kami-Ryotaro <kamiryo.0817@gmail.com>
2023-01-14 12:06:39 +0000

    [add]新規作成から新しいお知らせを投稿できるようにしました。

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,14 +3,27 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_path, notice: "プロフィールを登録しました。"
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :author)
   end
 end
