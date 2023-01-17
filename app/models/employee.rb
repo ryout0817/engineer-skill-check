e7667a411658d5cdb5dc3691b8fe099b4785bce4
Kami-Ryotaro <kamiryo.0817@gmail.com>
2023-01-17 05:12:09 +0000

    [delete]テーブル定義書にuniqunessの設定がなかったため削除しました。

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,12 +4,12 @@ class Employee < ApplicationRecord
   has_many :articles
   has_many :profiles
 
-  validates :number, presence: true, uniqueness: true
+  validates :number, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :account, presence: true, uniqueness: true
+  validates :account, presence: true
   validates :password, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true
   validates :date_of_joining, presence: true
 
   scope :active, -> {
